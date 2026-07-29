b036c9997fde91c67a50c396ce476784c63ac8fa
byeonjy <byeonjy8668@gmail.com>
2026-07-29 11:03:17 +0900

    엑셀차트 추가

--- excel/summary.py
+++ excel/summary.py
@@ -3,6 +3,7 @@
 import excel.styles as styles
 
 from collections import defaultdict # defaultdict: 카테고리별 합계를 만들 때 사용
+from openpyxl.chart import BarChart, Reference #BarChart: 막대 차트 객체, Reference: 어느 셀을 차트 데이터로 사용할지 지정 
 
 def create_summary_sheet(worksheet, money_data, budget):
     # 시트 이름
@@ -219,6 +220,40 @@ def create_summary_sheet(worksheet, money_data, budget):
         worksheet.row_dimensions[row_pay].height = 20
 
         row_pay += 1
+
+    # ----------------------------------------------------
+    # 6. 차트 데이터
+    # ----------------------------------------------------
+    chart_row = 2
+    
+    for category, price in category_data:
+        worksheet[f"H{chart_row}"] = category
+        worksheet[f"I{chart_row}"] = price
+
+        chart_row += 1
+
+    CATEGORY_COL = 8 # H열
+    PRICE_COL = 9 # I열
+
+    chart = BarChart() # 막대그래프 객체 생성
+
+    # 데이터 범위 지정 - 세로축(Y축) 데이터
+    data = Reference(worksheet, min_col=PRICE_COL, min_row=2, max_row=chart_row - 1) # Reference: 차트가 사용할 셀 범위 / min_col=9: 9번째 열(I열)
+
+    # 카테고리 범위 지정 - 가로축(X축)
+    categories = Reference(worksheet, min_col=CATEGORY_COL, min_row=2, max_row=chart_row - 1)
+
+    # 차트에 연결 (데이터 추가, 카테고리 추가)
+    chart.add_data(data)
+    chart.set_categories(categories)
+
+    # 차트 꾸미기
+    chart.title = "카테고리별 지출"
+    chart.width = 13
+    chart.height = 7
+
+    # 시트에 추가
+    worksheet.add_chart(chart, "A18") # A18: 왼쪽 위 시작 위치. 즉, A18부터 그래프가 그려짐
 
     # ----------------------------------------------------
     # 열 너비
